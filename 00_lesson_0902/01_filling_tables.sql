0388f7ab54e2d739ee5af4bcad63626e1ac0e33f
demidraksin <greatraksin@icloud.com>
2022-02-09 09:52:47 +0300

    Filling database with data 🍕

---
+++ 00_lesson_0902/01_filling_tables.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, email, birthday)
+VALUES ('Zodiac', 'zodiac@gmail.com', date('1989-02-03')),
+       ('JohnDoe', 'jdoe@gmail.com', date('1978-05-01')),
+       ('Valerie123', 'val_val@gmail.com', date('1999-01-02')),
+       ('JuliaRob15', 'julia_lol@example.com', date('1997-08-28')),
+       ('SaintKiller', 'vlad_first_blood@gmail.com', date('2009-03-18'));
+
